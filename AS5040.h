910cf3004e1c45cee282f49ac29d4ab0a86af629
John <smellsofbikes@users.noreply.github.com>
2014-07-18 15:43:23 -0600

    Create AS5040.h

---
+++ AS5040.h
@@ -0,0 +1,16 @@
+#ifndef AS5040_h
+#define AS5040_h
+
+#include "Arduino.h"
+
+class AS5040
+{
+  public:
+    AS5040(int ClockPin, int ChipSelectPin, int DataPin);
+    void init(int ClockPin, int ChipSelectPin, int DataPin);
+    long measured_degrees(void);
+    long measured_value(void);
+    int error(void);
+};
+
+#endif
