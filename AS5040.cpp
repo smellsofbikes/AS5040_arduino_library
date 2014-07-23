5a294707c08a72cc1b6f7ac690279d30d02c528c
John <smellsofbikes@users.noreply.github.com>
2014-07-23 15:45:13 -0600

    Update AS5040.cpp
    Major changes throughout, filled in read_chip to make it work, filled in error.

--- AS5040.cpp
+++ AS5040.cpp
@@ -2,61 +2,61 @@
 #include "AS5040.h"
 
 AS5040::AS5040(int ClockPin, int ChipSelectPin, int DataPin)
+    : _clock(ClockPin), _cs(ChipSelectPin), _data(DataPin)
 {
-  init(ClockPin, ChipSelectPin, DataPin);
+    pinMode(_clock, OUTPUT);
+    pinMode(_cs, OUTPUT);
+    pinMode(_data, INPUT);
 }
 
-void AS5040::init(int ClockPin, int ChipSelectPin, int DataPin)
-{
-  _clock = ClockPin;
-  _cs = ChipSelectPin;
-  _data = DataPin;
-  pinMode(ClockPin, OUTPUT);
-  pinMode(ChipSelectPin, OUTPUT);
-  pinMode(DataPin, INPUT);
-}
 
 long AS5040::measured_degrees(void)
 {
-  long measured_degrees;
-  long raw_value;
-  long anglemask = 262080; // 0x111111111111000000: mask to obtain first 12
-                           //  digits with position info
-
-  raw_value = read_chip();
-  raw_value = raw_value & anglemask; // mask out error
-  raw_value = (raw_value >> 6);      // shift over
-  measured_degrees = raw_value * 0.352; // angle * (360/1024)
-  return measured_degrees;
+  return (measured_value() * (360/1024));
 }
 
 long AS5040::measured_value(void)
 {
   long raw_value;
-  long anglemask = 262080; // 0x111111111111000000: mask to obtain first 12
-                           //  digits with position info
   raw_value = read_chip();
-  raw_value = raw_value & anglemask; // mask out error
   raw_value = (raw_value >> 6);      // shift over
-
   return raw_value;
+  // it's possible this could just be return (read_chip() >> 6);
 }
 
 int AS5040::error(void)
 {
-  int error_code = 0;  // not yet implemented
+  int error_code;  // not yet implemented
+  long raw_value;
+  raw_value = read_chip();
+  error_code = raw_value & 0x0000000000111111;
   return error_code;
 }
 
-int AS5040:read_chip(void)
+long AS5040::read_chip(void)
 {
   long raw_value;
+  int inputstream = 0;
+  int c;
+  digitalWrite(_cs, HIGH);
+  digitalWrite(_clock, HIGH);
+  delay(100);
+  digitalWrite(_cs, LOW);
+  delay(10);
+  digitalWrite(_clock, LOW);
+  delay(10);
+  for (c = 0; c < 16; c++)
+  {
+    digitalWrite(_clock, HIGH);
+    delay(10);
+    inputstream = digitalRead(_data);
+    raw_value == ((raw_value << 1) + inputstream);
+    digitalWrite(_clock, LOW);
+    delay(10);
+  }
   return raw_value;
 }
   
-
-
-
 
 
 
