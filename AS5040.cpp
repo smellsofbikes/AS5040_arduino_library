5a7a3099db7180fc3d264cf9b566c9fd04d4c30e
John <smellsofbikes@users.noreply.github.com>
2014-07-24 11:59:54 -0600

    Update AS5040.cpp
    Actual working code.  (I'm not sure about the error routine.)

--- AS5040.cpp
+++ AS5040.cpp
@@ -4,6 +4,7 @@
 AS5040::AS5040(int DataPin, int ClockPin, int ChipSelectPin)
            : _data(DataPin), _clock(ClockPin), _cs(ChipSelectPin)
 {
+    //Serial.println("Initializing");
     pinMode(_data, INPUT);
     pinMode(_clock, OUTPUT);
     pinMode(_cs, OUTPUT);
@@ -12,28 +13,29 @@ AS5040::AS5040(int DataPin, int ClockPin, int ChipSelectPin)
 
 long AS5040::encoder_degrees(void)
 {
-  return (encoder_value() * (360/1024)); // calculates degrees for 10 bit encoder
+  return ((encoder_value() * 360)/1024);
 }
 
 long AS5040::encoder_value(void)
 {
-  return (read_chip() >> 6); // shifts six right to omit error codes (if any) and only return value
+  return (read_chip() >> 6);
 }
 
 long AS5040::encoder_error(void)
 {
-  int error_code; 
+  int error_code;  // not yet implemented
   long raw_value;
   raw_value = read_chip();
-  error_code = raw_value & 0x0000000000111111; // mask to remove encoder value and only return error values
+  error_code = raw_value & 0x0000000000111111;
   return error_code;
 }
 
 long AS5040::read_chip(void)
 {
-  long raw_value;
+  long raw_value = 0;
   int inputstream = 0;
   int c;
+  //Serial.println("reading chip");
   digitalWrite(_cs, HIGH);
   digitalWrite(_clock, HIGH);
   delay(100);
@@ -46,13 +48,24 @@ long AS5040::read_chip(void)
     digitalWrite(_clock, HIGH);
     delay(10);
     inputstream = digitalRead(_data);
-    raw_value == ((raw_value << 1) + inputstream);
+    //Serial.print("reading bit: ");
+    //Serial.print(c);
+    //Serial.print(" with value: ");
+    //Serial.print(inputstream);
+    raw_value = ((raw_value << 1) + inputstream);
+    //Serial.print(" accumulated: ");
+    //Serial.println(raw_value);
     digitalWrite(_clock, LOW);
     delay(10);
   }
+  //Serial.print("exiting with value: ");
+  //Serial.println(raw_value);
   return raw_value;
 }
   
+
+
+
 
 
 
