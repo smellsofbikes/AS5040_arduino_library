8c28a7b243a671c9393563b843dc3ede466c0aac
John <smellsofbikes@users.noreply.github.com>
2014-07-24 15:32:13 -0600

    Update AS5040.cpp
    Functional program with error reporting

--- AS5040.cpp
+++ AS5040.cpp
@@ -4,7 +4,6 @@
 AS5040::AS5040(uint16_t DataPin, uint16_t ClockPin, uint16_t ChipSelectPin)
            : _data(DataPin), _clock(ClockPin), _cs(ChipSelectPin)
 {
-    //Serial.println("Initializing");
     pinMode(_data, INPUT);
     pinMode(_clock, OUTPUT);
     pinMode(_cs, OUTPUT);
@@ -26,11 +25,12 @@ uint32_t AS5040::encoder_error(void)
   uint16_t error_code;  // not yet implemented
   uint32_t raw_value;
   raw_value = read_chip();
-  Serial.print("raw code: ");
-  Serial.print(raw_value);
   error_code = raw_value & 0b0000000000111111;
-  Serial.print("clipped: ");
-  Serial.println(error_code);
+  err_value.DECn = error_code & 2;
+  err_value.INCn = error_code & 4;
+  err_value.LIN = error_code & 8;
+  err_value.COF = error_code & 16;
+  err_value.OCF = error_code & 32;
   return error_code;
 }
 
@@ -39,7 +39,6 @@ uint32_t AS5040::read_chip(void)
   uint32_t raw_value = 0;
   uint16_t inputstream = 0;
   uint16_t c;
-  //Serial.println("reading chip");
   digitalWrite(_cs, HIGH);
   digitalWrite(_clock, HIGH);
   delay(100);
@@ -52,29 +51,12 @@ uint32_t AS5040::read_chip(void)
     digitalWrite(_clock, HIGH);
     delay(10);
     inputstream = digitalRead(_data);
-    //Serial.print("reading bit: ");
-    //Serial.print(c);
-    //Serial.print(" with value: ");
-    //Serial.print(inputstream);
     raw_value = ((raw_value << 1) + inputstream);
-    //Serial.print(" accumulated: ");
-    //Serial.println(raw_value);
     digitalWrite(_clock, LOW);
     delay(10);
   }
-  //Serial.print("exiting with value: ");
-  //Serial.println(raw_value);
-  return raw_value;
+ return raw_value;
 }
   
-
-
-
-
-
-
-
-
-
 
 
